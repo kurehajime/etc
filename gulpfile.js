248542f37d21d294a79a31df7637a9129da9d426
kurehajime <x.iidec+gh@googlemail.com>
2016-06-14 21:35:05 +0900

    timestamp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,30 @@
 var gulp = require('gulp');
 var fs = require('fs');
-var uglify = require('gulp-uglify');
 var inlinesource = require('gulp-inline-source');
+var uglify = require('gulp-uglify');
+var replace = require('gulp-replace');
+var date=require('date-utils');
+
 
 gulp.task('colamone', function() {
-    
+    var dt = new Date();
+    var formatted = dt.toFormat("YYYYMMDDHH24MI");
     //index.html作成
-    fs.createReadStream('../colamone_js/colamone.html')
+    fs.createReadStream('../colamone_js/colamone.html')        
         .pipe(fs.createWriteStream('../colamone_js/index.html'));
+    gulp.src( '../colamone_js/index.html'  )
+        .pipe(replace(/TIMESTAMP/g, formatted))
+        .pipe( gulp.dest( '../colamone_js') )
+
     
     //公開フォルダへコピー
     gulp.src( '../colamone_js/*.html'  )
         .pipe(inlinesource())
+        .pipe(replace(/TIMESTAMP/g, formatted))
         .pipe( gulp.dest( '../xiidec/static/colamone') )
         .pipe( gulp.dest( '../xiidec/static/colamone_beta') );
     gulp.src( '../colamone_js/*.js'  )
+        .pipe(uglify())
         .pipe( gulp.dest( '../xiidec/static/colamone') )
         .pipe( gulp.dest( '../xiidec/static/colamone_beta') );
     gulp.src( '../colamone_js/*.css'  )
@@ -28,6 +38,5 @@ gulp.task('colamone', function() {
 gulp.task('watch', function() {
   gulp.watch('../colamone_js/*', ['colamone']);
 });
-
 
 gulp.task('default', ['colamone']);
